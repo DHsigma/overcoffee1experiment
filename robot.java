4a0557bc11f5404ae24f78043248cf35f54124e3
DHsigma <480701@bsd48.org>
2025-01-18 17:10:42 -0800

    change ticks per rev.java
    changing ticks per rev to spin faster and more precise

--- robot.java
+++ robot.java
@@ -56,7 +56,7 @@ public class Auto_Score_Specimen_Middle extends LinearOpMode {
 
 
     // this is ALMOST the best code
-    private static final double TICKS_PER_REV = 28 * (((1+(46.0/17)))*(1+(46.0/11))); // NEED TO CHANGE
+    private static final double TICKS_PER_REV = 30 * (((1+(46.0/17)))*(1+(46.0/11))); // NEED TO CHANGE
     private static final double WHEEL_DIAMETER_MILLIMETER = 104; // Diameter of the wheel in millimeter
     private static final double CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER_MILLIMETER; // Circumference
     private static final double TICKS_PER_MILLIMETER = TICKS_PER_REV / CIRCUMFERENCE;
